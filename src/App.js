cb3ea490bbea48de8d17a528b50a5db0585e25f5
松尾康平 <matsuo.work.0@gmail.com>
2025-03-19 17:52:53 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+const NameText = (props) => {
+  return (
+    <p>Hi. {props.name}!</p>
+  )
+}
+
+const Profile = (props) => {
+  return (
+    <div>
+      <p>{props.age}</p>
+      <p>{props.country}</p>
+    </div>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NameText name="Jack" />
+      <Profile age={22} country="US" />
+      <NameText name="Mary" />
+      <Profile age={20} country="UK" />
     </div>
   );
 }
