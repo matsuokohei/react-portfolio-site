94cd964868d0c9918f8c4be5991e1e0404638dc1
松尾康平 <matsuo.work.0@gmail.com>
2025-03-21 16:27:25 +0900

    refactor

--- src/customHooks/useSkills.js
+++ src/customHooks/useSkills.js
@@ -1,13 +1,13 @@
 import { useEffect, useReducer } from "react";
 import axios from 'axios'
 import { skillReducer, initialState, actionTypes } from "../reducers/skillReducer";
+import { requestStates } from "../constants";
 
 export const useSkills = () => {
 
   const [state, dispatch] = useReducer(skillReducer, initialState);
 
-  useEffect(() => {
-    dispatch({type: actionTypes.fetch});
+  const fetchReposApi = () => {
     axios.get('https://api.github.com/users/matsuokohei/repos')
       .then((response) => {
         const languageList = response.data.map(res => res.language);
@@ -17,7 +17,14 @@ export const useSkills = () => {
       .catch(() => {
         dispatch({type: actionTypes.error});
       });
-  }, [])
+  };
+
+  useEffect(() => {
+    if (state.requestState !== requestStates.loading) return;
+    fetchReposApi();
+  }, [state.requestState]);
+
+  useEffect(() => dispatch({type: actionTypes.fetch}), [])
 
   const generateLanguageCountObj = (allLanguageList) => {
     const notNullLanguageList = allLanguageList.filter(language => language != null);
@@ -28,9 +35,12 @@ export const useSkills = () => {
     }));
   };
 
-  const convertCountToPercentage = (count) => {
-    if (count > 10) return 100;
-    return count * 10;
+  const DEFAULT_MAX_PERCENTAGE = 100;
+  const LANGUAGE_COUNT_BASE = 10;
+
+  const convertCountToPercentage = (languageCount) => {
+    if (languageCount > LANGUAGE_COUNT_BASE) return DEFAULT_MAX_PERCENTAGE;
+    return languageCount * LANGUAGE_COUNT_BASE;
   };
 
   const sortedLanguageList = () => (
