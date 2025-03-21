6d2b83a8611e0c1cedec05ac9b9d3c0e98c34a0e
松尾康平 <matsuo.work.0@gmail.com>
2025-03-21 15:36:33 +0900

    fix portfolio

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,12 @@
+import { CircularProgressbar } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
+import { requestStates } from "../constants";
+import { useSkills } from '../customHooks/useSkills';
+
 export const Skills = () => {
+
+  const [sortedLanguageList, fetchRequestState, convertCountToPercentage] = useSkills();
+
   return (
     <div id="skills">
       <div className="container">
@@ -6,6 +14,26 @@ export const Skills = () => {
           <h2>Skills</h2>
         </div>
         <div className="skills-container">
+          {
+            fetchRequestState === requestStates.loading && (
+              <p className="description">取得中</p>
+            )
+          }
+          {
+            fetchRequestState === requestStates.success && (
+              sortedLanguageList().map((item, index) => (
+                <div className='skill-item' key={index}>
+                  <p className='description'><strong>{item.language}</strong></p>
+                  <CircularProgressbar value={convertCountToPercentage(item.count)} text={`${convertCountToPercentage(item.count)}%`} />
+                </div>
+              ))
+            )
+          }
+          {
+            fetchRequestState === requestStates.error && (
+              <p className="description">エラーが発生しました</p>
+            )
+          }
         </div>
       </div>
     </div>
